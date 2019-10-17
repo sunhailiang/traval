db6fc022e7bcae0d3589f8f2d2f4348b3938d449
ËﺣÁÁ <sunhl@Fosun.com>
2019-10-18 00:38:51 +0800

    icons

---
+++ src/pages/home/components/Recomment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecomment'
+}
+</script>
+<style lang="stylus" scoped>
+.recommend-title
+  background-color #eeeeee
+  line-height 0.8rem
+  text-indent 0.2rem
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,14 +3,16 @@
     <HomeHeader/>
     <HomeSwiper/>
     <HomeIcons/>
+    <HomeRecomment/>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecomment from './components/Recomment'
 export default {
   name: 'Home',
-  components: { HomeHeader, HomeSwiper, HomeIcons }
+  components: { HomeHeader, HomeSwiper, HomeIcons, HomeRecomment }
 }
 </script>
