c3c1f63aa4d79bc5b00df41ac476126994c37644
ËﺣÁÁ <sunhl@Fosun.com>
2019-10-20 01:34:36 +0800

    weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="weekend-title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom"
+          :key="item.id"
+          v-for="item of weekendList">
+        <div class="item-wraper-img">
+          <img class="item-img"
+               :src='item.imgUrl'
+               alt="">
+        </div>
+        <div class="item-wraper-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '01',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/387c5e767cccc7a3172ed7ea15b50727.png',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫得海洋主题乐园'
+      }, {
+        id: '02',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/387c5e767cccc7a3172ed7ea15b50727.png',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫得海洋主题乐园'
+      }, {
+        id: '03',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/387c5e767cccc7a3172ed7ea15b50727.png',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫得海洋主题乐园'
+      }]
+    }
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+.weekend-title
+  clear both
+  text-align left
+  background-color #eeeeee
+  line-height 0.8rem
+  text-indent 0.2rem
+.item-wraper-img
+  padding 0.25rem
+.item-wraper-info
+  padding 0.3rem
+.item-img
+  width 100%
+  height 2rem
+.item-title
+  font-size 0.35rem
+.item-desc
+  font-size 0.3rem
+  color gray
+  margin-top 0.15rem
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,6 +4,7 @@
     <HomeSwiper/>
     <HomeIcons/>
     <HomeRecomment/>
+    <HomeWeekend/>
   </div>
 </template>
 <script>
@@ -11,8 +12,9 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecomment from './components/Recomment'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
-  components: { HomeHeader, HomeSwiper, HomeIcons, HomeRecomment }
+  components: { HomeHeader, HomeSwiper, HomeIcons, HomeRecomment, HomeWeekend }
 }
 </script>
